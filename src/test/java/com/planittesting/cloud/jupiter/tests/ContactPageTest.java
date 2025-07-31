0785be92164fa88a70d088d5a3a3a19348ea8774
ailn.t <aillenmudrey@gmail.com>
2025-07-31 15:56:48 +1200

    Work for Test Case 3

--- src/test/java/com/planittesting/cloud/jupiter/tests/ContactPageTest.java
+++ src/test/java/com/planittesting/cloud/jupiter/tests/ContactPageTest.java
@@ -9,12 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ContactPageTest extends BaseTest {
+
     private static final Logger logger = Logger.getLogger(ContactPageTest.class.getName());
+
+    // Expected fields validation messages
+    private static final String EXPECTED_FORENAME_REQUIRED_MESSAGE = "Forename is required";
+    private static final String EXPECTED_EMAIL_REQUIRED_MESSAGE = "Email is required";
+    private static final String EXPECTED_MESSAGE_REQUIRED_MESSAGE = "Message is required";
+
+    // Invalid messages
+    private static final String INVALID_EMAIL_TEXT = "thisisnotavalidemail";
+    private static final String EXPECTED_INVALID_EMAIL_TEXT = "Please enter a valid email";
+
+    // Valid field values
+    private static final String VALID_EMAIL = "test@planittesting.com";
+    private static final String VALID_FORENAME = "Test";
+    private static final String VALID_MESSAGE = "Our test bears won’t let a single bug through the toy aisle.";
 
     @Test
     public void enterInvalidEmailTest() {
-        String invalidEmailText = "thisisnotavalidemail";
-        String expectedInvalidEmailText = "Please enter a valid email";
 
         ContactPage contactPage = new ContactPage(driver);
 
@@ -23,24 +36,17 @@ public class ContactPageTest extends BaseTest {
         logger.info("Navigated to the Contact page.");
 
         // Step 2: Populate the email field with thisisnotavalidemail
-        contactPage.enterEmail(invalidEmailText);
-        logger.info("Entered invalid email: " + invalidEmailText);
+        contactPage.enterEmail(INVALID_EMAIL_TEXT);
+        logger.info("Entered invalid email: " + INVALID_EMAIL_TEXT);
 
         // Step 3: Verify that the email error is displayed with text:
         String actualErrorMessage = contactPage.getEmailErrorMessage();
-        assertEquals(expectedInvalidEmailText, actualErrorMessage, "Error message displayed when email is invalid");
+        assertEquals(EXPECTED_INVALID_EMAIL_TEXT, actualErrorMessage, "Error message displayed when email is invalid");
 
     }
 
     @Test
     public void submitFormWithEmptyFieldsTest() {
-        String expectedForenameRequiredMessage = "Forename is required";
-        String expectedEmailRequiredMessage = "Email is required";
-        String expectedMessageRequiredMessage = "Message is required";
-
-        String validEmail = "test@planittesting.com";
-        String validForename = "Test";
-        String validMessage = "Our test bears won’t let a single bug through the toy aisle.";
 
         ContactPage contactPage = new ContactPage(driver);
 
@@ -58,18 +64,40 @@ public class ContactPageTest extends BaseTest {
         String actualMessageErrorMessage = contactPage.getMessageErrorMessage();
 
         assertAll("Validation of messages displayed for required fields",
-                () -> assertEquals(expectedForenameRequiredMessage, actualForenameErrorMessage, "Error message displayed when Forename is empty"),
-                () -> assertEquals(expectedEmailRequiredMessage, actualEmailErrorMessage, "Error message displayed when Email is empty"),
-                () -> assertEquals(expectedMessageRequiredMessage, actualMessageErrorMessage, "Error message displayed when Message is empty")
+                () -> assertEquals(EXPECTED_FORENAME_REQUIRED_MESSAGE, actualForenameErrorMessage, "Error message displayed when Forename is empty"),
+                () -> assertEquals(EXPECTED_EMAIL_REQUIRED_MESSAGE, actualEmailErrorMessage, "Error message displayed when Email is empty"),
+                () -> assertEquals(EXPECTED_MESSAGE_REQUIRED_MESSAGE, actualMessageErrorMessage, "Error message displayed when Message is empty")
         );
 
         // Step 4: Populate mandatory fields
-        contactPage.populateMandatoryFields(validEmail, validForename, validMessage);
+        contactPage.populateMandatoryFields(VALID_EMAIL, VALID_FORENAME, VALID_MESSAGE);
         logger.info("Populated mandatory fields.");
 
         // Step 5: Validate that the mandatory errors are no longer displayed
         contactPage.waitUntilRequiredMessagesNotVisible();
         logger.info("Error messages for required fields are no longer shown.");
     }
-}
 
+    @Test
+    public void submitValidContactFormTest() {
+
+        String expectedThanksMessage = String.format("Thanks %s, we appreciate your feedback.", VALID_FORENAME);
+
+        ContactPage contactPage = new ContactPage(driver);
+
+        // Step 1: From the home page go to the contact page
+        contactPage.openContactPage();
+        logger.info("Navigated to the Contact page.");
+
+        // Step 2: Populate the mandatory fields
+        contactPage.populateMandatoryFields(VALID_EMAIL, VALID_FORENAME, VALID_MESSAGE);
+
+        // Step 3: Click on the Submit button
+        contactPage.submitForm();
+        logger.info("Clicked the Submit button with mandatory fields populated.");
+
+        // Step 4: Verify Thanks message text
+        String actualThanksMessage = contactPage.getSubmissionText();
+        assertEquals(expectedThanksMessage, actualThanksMessage, "Submission message correctly displayed");
+    }
+}

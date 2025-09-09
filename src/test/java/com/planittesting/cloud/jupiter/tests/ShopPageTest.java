82e848b8635b81855bab061eb2aa5a32a796c828
ailn.t <aillenmudrey@gmail.com>
2025-09-10 11:15:21 +1200

    removing unused import

--- src/test/java/com/planittesting/cloud/jupiter/tests/ShopPageTest.java
+++ src/test/java/com/planittesting/cloud/jupiter/tests/ShopPageTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ShopPageTest extends BaseTest {
 
